f09e7199846d7bbd98fac4ca936bc7d6d55c1295
Andrey <luchko.andrey@gmail.com>
2023-06-28 21:51:40 +0300

    refactor: change root to colors & variable throw $

--- src/pageComponents/HomePage/blocks/AboutUs/AboutUs.tsx
+++ src/pageComponents/HomePage/blocks/AboutUs/AboutUs.tsx
@@ -1,13 +1,13 @@
 import { Container } from '~components/Container/Container';
 import { Section } from '~components/Section/Section';
 
-import style from './AboutUs.module.scss';
+import s from './AboutUs.module.scss';
 
 export function AboutUs() {
 	return (
-		<Section className={style.AboutUs}>
+		<Section className={s.AboutUs}>
 			<Container>
-				<h2>AboutUs</h2>
+				<h2 className={s.H1}>AboutUs</h2>
 			</Container>
 		</Section>
 	);
