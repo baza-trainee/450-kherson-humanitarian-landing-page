b0f2971dd4e3ce9e7a142a5039e96c3cc369e9be
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-07-07 16:18:33 +0300

    refactor: change color variables

--- src/components/Buttons/TextButton.tsx
+++ src/components/Buttons/TextButton.tsx
@@ -6,18 +6,19 @@ import s from './TextButton.module.scss';
 
 type ButtonElement = HTMLElementTagNameMap['button'];
 interface ButtonProps extends React.HTMLAttributes<ButtonElement> {
-	size?: ' medium' | 'big';
+	size?: 'medium' | 'big';
 	disabled?: boolean;
-	type?: 'fill' | 'outline' | 'disabled';
+	type?: 'fill' | 'outline';
 	submit?: boolean;
 }
 
 export const Button = forwardRef<ButtonElement, ButtonProps>(
-	({ size = 'medium', type = 'fill', submit, children, className, ...rest }, ref) => {
+	({ size = 'medium', type = 'fill', submit, disabled, children, className, ...rest }, ref) => {
+		const componentClass = [disabled && s.disabled, size && s[size], type && s[type]];
 		return (
 			<button
 				type={submit ? 'submit' : 'button'}
-				className={clsx(s.Button, className, `s.${size}`, `s.${type}`)}
+				className={clsx(s.Button, className, componentClass)}
 				ref={ref}
 				{...rest}
 			>
