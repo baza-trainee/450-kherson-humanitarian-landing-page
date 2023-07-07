4d27e03ae268d5ede436a314e3116c8a8cfbc23a
Andrey <luchko.andrey@gmail.com>
2023-07-07 20:19:43 +0300

    refactor: remove wrong code

--- src/pageComponents/HomePage/blocks/AboutUs/AboutUs.tsx
+++ src/pageComponents/HomePage/blocks/AboutUs/AboutUs.tsx
@@ -1,6 +1,5 @@
 import { Container } from '~components/Container/Container';
 import { Section } from '~components/Section/Section';
-import { Text } from '~components/Text/Text';
 
 import s from './AboutUs.module.scss';
 
@@ -9,18 +8,6 @@ export function AboutUs() {
 		<Section className={s.AboutUs}>
 			<Container>
 				<h2>AboutUs</h2>
-				<Text variant="h1">h1 Check</Text>
-				<Text variant="h2">h2 Check</Text>
-				<Text variant="h3">h3 Check</Text>
-				<Text variant="h4">h4 Check</Text>
-				<Text variant="subtitle">subtitle-h6 Check</Text>
-				<Text variant="p">p Check</Text>
-				<Text variant="button">button Check</Text>
-				<Text variant="header">header Check</Text>
-				<Text variant="footer">footer Check</Text>
-				<Text variant="various1">various1 Check</Text>
-				<Text variant="various2">various2 Check</Text>
-				<Text variant="various3">various3 Check</Text>
 			</Container>
 		</Section>
 	);
