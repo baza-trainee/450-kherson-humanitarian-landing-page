0994abb7f36f30aa2dcc25ca722e010bfd869d6f
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-07-08 10:12:39 +0300

    refactor: change component name

--- src/components/Buttons/ArrowUpButton.tsx
+++ src/components/Buttons/ArrowUpButton.tsx
@@ -7,12 +7,12 @@ import s from './ArrowUpButton.module.scss';
 type ButtonElement = HTMLElementTagNameMap['button'];
 type ButtonProps = React.HTMLAttributes<ButtonElement>;
 
-export const Button = forwardRef<ButtonElement, ButtonProps>(({ children, className, ...rest }, ref) => {
+export const ArrowUpButton = forwardRef<ButtonElement, ButtonProps>(({ children, className, ...rest }, ref) => {
 	return (
 		<button className={clsx(s.Button, className)} ref={ref} {...rest}>
-			<span className={clsx(s.label)}>{children}</span>
+			<span className={s.label}>{children}</span>
 		</button>
 	);
 });
 
-Button.displayName = 'Button';
+ArrowUpButton.displayName = 'Button';
