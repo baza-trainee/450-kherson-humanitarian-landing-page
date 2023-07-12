009e1ccb74842e3e997031ccff3599762e23f033
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-07-12 15:30:56 +0300

    feature: add hero block

--- src/pageComponents/HomePage/blocks/Hero/Hero.tsx
+++ src/pageComponents/HomePage/blocks/Hero/Hero.tsx
@@ -1,14 +1,145 @@
+import React, { useState } from 'react';
+
+import clsx from 'clsx';
+import { useKeenSlider } from 'keen-slider/react';
+
+import { Button } from '~components/Buttons/Button';
 import { Container } from '~components/Container/Container';
-import { Section } from '~components/Section/Section';
+import { Icon } from '~components/Icon/Icon';
+import { Text } from '~components/Text/Text';
+import { useScreenQuery } from '~hooks/useScreenQuery';
+
+import 'keen-slider/keen-slider.min.css';
 
 import s from './Hero.module.scss';
 
-export function Hero() {
+function Arrow(props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) {
 	return (
-		<Section className={s.Hero}>
-			<Container>
-				<h2>Hero</h2>
-			</Container>
-		</Section>
+		<button className={s.arrow}>
+			{props.left && (
+				<Icon
+					icon="icon--arrow-left"
+					colors={{ default: 'var(--color--primary-3)' }}
+					onClick={props.onClick}
+					disabled={props.disabled}
+				></Icon>
+			)}
+			{!props.left && (
+				<Icon
+					icon="icon--arrow-right"
+					colors={{ default: 'var(--color--primary-3)' }}
+					onClick={props.onClick}
+					disabled={props.disabled}
+				></Icon>
+			)}
+		</button>
+	);
+}
+
+export function Hero() {
+	const [currentSlide, setCurrentSlide] = React.useState(0);
+	const [loaded, setLoaded] = useState(false);
+	const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+		initial: 0,
+		slideChanged(slider) {
+			setCurrentSlide(slider.track.details.rel);
+		},
+		created() {
+			setLoaded(true);
+		},
+	});
+	const { isScreenTabletSm } = useScreenQuery();
+	const buttons = (
+		<div className={s.buttonsGap}>
+			<Button type="primary">Отримати допомогу</Button>
+			<Button type="secondary">Допомогти нам</Button>
+		</div>
+	);
+	const dots = (
+		<>
+			<div className={s.column}>{buttons}</div>
+			{loaded && instanceRef.current && (
+				<div className={s.dots}>
+					{[...Array(instanceRef.current.track.details.slides.length).keys()].map((idx) => {
+						return (
+							<button
+								key={idx}
+								onClick={() => {
+									instanceRef.current?.moveToIdx(idx);
+								}}
+								className={clsx(s.dot, currentSlide === idx ? `${s.active}` : '')}
+							></button>
+						);
+					})}
+				</div>
+			)}
+		</>
+	);
+
+	const arrows = (
+		<div className={s.row}>
+			{buttons}
+			{loaded && instanceRef.current && (
+				<div className={s.arrowsGap}>
+					<Arrow
+						left
+						onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev()}
+						disabled={currentSlide === 0}
+					/>
+
+					<Arrow
+						onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next()}
+						disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
+					/>
+				</div>
+			)}
+		</div>
+	);
+	console.log(isScreenTabletSm);
+	function renderNavigation() {
+		return isScreenTabletSm ? dots : arrows;
+	}
+	return (
+		<Container>
+			<div ref={sliderRef} className="keen-slider">
+				<div className="keen-slider__slide">
+					<div className={clsx(s.content, s.banner1)}>
+						<div className={s.text}>
+							<Text variant="h1" className={s.heading}>
+								Надаємо гуманітарні набори потребуючим
+							</Text>
+							<Text variant="various3">м. Кривий Ріг</Text>
+						</div>
+						{renderNavigation()}
+					</div>
+				</div>
+				<div className="keen-slider__slide">
+					<div className={clsx(s.content, s.banner2)}>
+						<div className={s.text}>
+							<Text variant="h1" className={clsx(s.whiteColor, s.headingWidth)}>
+								Відбудовуємо зруйновані та пошкоджені об’єкти (Херсонська область)
+							</Text>
+							<Text variant="various3" className={s.whiteColor}>
+								м. Кривий Ріг
+							</Text>
+						</div>
+						{renderNavigation()}
+					</div>
+				</div>
+				<div className="keen-slider__slide">
+					<div className={clsx(s.content, s.banner3)}>
+						<div className={s.text}>
+							<Text variant="h1" className={clsx(s.whiteColor, s.headingWidth)}>
+								Забезпечуємо медичні заклади м. Кривий Ріг, Криворізького району та Херсонської області
+							</Text>
+							<Text variant="various3" className={s.whiteColor}>
+								м. Кривий Ріг
+							</Text>
+						</div>
+						{renderNavigation()}
+					</div>
+				</div>
+			</div>
+		</Container>
 	);
 }
