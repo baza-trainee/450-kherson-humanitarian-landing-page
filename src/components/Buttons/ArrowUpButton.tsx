ac03575f40251de8955e9e369d5f9f03731b92d4
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-07-14 21:55:38 +0300

    refactor: arrowButton works as it should
    add additional condition to hide button in footer

--- src/components/Buttons/ArrowUpButton.tsx
+++ src/components/Buttons/ArrowUpButton.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react';
+import { useEffect, useState } from 'react';
 
 import clsx from 'clsx';
 
@@ -8,8 +9,51 @@ type ButtonElement = HTMLElementTagNameMap['button'];
 type ButtonProps = React.HTMLAttributes<ButtonElement>;
 
 export const ArrowUpButton = forwardRef<ButtonElement, ButtonProps>(({ children, className, ...rest }, ref) => {
+	const [showButton, setShowButton] = useState(false);
+
+	const handleScrollUp = () => {
+		setShowButton(window.scrollY > document.documentElement.clientHeight);
+	};
+
+	const handleScrollDown = () => {
+		const FooterSection = document.getElementById('Footer');
+		if (FooterSection) {
+			const sectionRect = FooterSection.getBoundingClientRect();
+			const windowHeight = window.innerHeight;
+			setShowButton(sectionRect.top > windowHeight);
+		}
+	};
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const getHelpSection = document.getElementById('GetHelp');
+			if (getHelpSection) {
+				const sectionRect = getHelpSection.getBoundingClientRect();
+				const windowHeight = window.innerHeight;
+
+				if (sectionRect.top <= windowHeight) {
+					handleScrollDown();
+				} else {
+					handleScrollUp();
+				}
+			}
+		};
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<button className={clsx(s.Button, className)} ref={ref} {...rest}>
+		<button
+			className={clsx(s.Button, className, !showButton && s.hidden)}
+			onClick={() => {
+				document.documentElement.scrollIntoView({ block: 'start' });
+			}}
+			ref={ref}
+			{...rest}
+		>
 			{children}
 		</button>
 	);
