cb5bcc329ec079f20bfb8675c077a60042a43f78
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-07-21 23:10:59 +0300

    feat: add Form block

--- src/pageComponents/HomePage/blocks/GetHelp/GetHelp.tsx
+++ src/pageComponents/HomePage/blocks/GetHelp/GetHelp.tsx
@@ -1,6 +1,8 @@
 import { Container } from '~components/Container/Container';
 import { Section } from '~components/Section/Section';
 
+import { Form } from './Form/Form';
+
 import s from './GetHelp.module.scss';
 
 export function GetHelp() {
@@ -8,6 +10,10 @@ export function GetHelp() {
 		<Section className={s.GetHelp}>
 			<Container>
 				<h2>GetHelp</h2>
+				<div className={s.content}>
+					<Form />
+					<div className={s.info} />
+				</div>
 			</Container>
 		</Section>
 	);
