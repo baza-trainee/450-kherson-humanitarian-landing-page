2d00f3e3a2c49bb15695e38c970e527ea7cbca77
Andrey <luchko.andrey@gmail.com>
2023-08-15 17:13:02 +0300

    refactor: separated callback handleDragStart

--- src/pageComponents/HomePage/blocks/OurActivity/Carousel3d/Carousel3d.tsx
+++ src/pageComponents/HomePage/blocks/OurActivity/Carousel3d/Carousel3d.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode } from 'react';
 
+import type { MotionProps } from 'framer-motion';
 import { AnimatePresence, motion } from 'framer-motion';
 
 import { useHandleDrag } from '~hooks/useHandleDrag';
@@ -15,14 +16,14 @@ interface CardData {
 interface SliderProps {
 	className: string;
 	renderContent: (src: string, title?: string, text?: string) => ReactNode;
-	animate: object;
+	animate: MotionProps;
 	screens?: Record<string, boolean>;
 	paginate: (dir: number) => void;
 	direction: number;
 	visibleIndices: CardData[];
 }
 
-export function Carousel({
+export function Carousel3d({
 	className,
 	renderContent,
 	animate,
@@ -44,37 +45,35 @@ export function Carousel({
 		}
 	};
 
-	const { handleDrag } = useHandleDrag(
+	const { handleDragEnd, handleDragStart } = useHandleDrag(
 		() => paginate(1),
 		() => paginate(-1),
 	);
 
 	return (
-		<>
-			<div className={className}>
-				<AnimatePresence mode="popLayout" custom={direction} initial={false}>
-					{visibleIndices.map((card) => {
-						const { src, title, text } = card;
-						return (
-							<motion.div
-								key={src}
-								className={s.cardWrapper}
-								layout
-								custom={{
-									slidePosition: getImageIndex(src),
-									direction,
-									...screens,
-								}}
-								{...animate}
-								onDragStart={(e) => ((e.target as HTMLDivElement).style.cursor = 'grabbing')}
-								onDragEnd={handleDrag}
-							>
-								{renderContent(src, title, text)}
-							</motion.div>
-						);
-					})}
-				</AnimatePresence>
-			</div>
-		</>
+		<div className={className}>
+			<AnimatePresence mode="popLayout" custom={direction} initial={false}>
+				{visibleIndices.map((card) => {
+					const { src, title, text } = card;
+					return (
+						<motion.div
+							key={src}
+							className={s.cardWrapper}
+							layout
+							custom={{
+								slidePosition: getImageIndex(src),
+								direction,
+								...screens,
+							}}
+							{...animate}
+							onDragStart={handleDragStart}
+							onDragEnd={handleDragEnd}
+						>
+							{renderContent(src, title, text)}
+						</motion.div>
+					);
+				})}
+			</AnimatePresence>
+		</div>
 	);
 }
