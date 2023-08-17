d6026c89bf0eb4a0395c1a651e23a6f382d02b98
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-17 18:27:57 +0300

    refactor: ButtonHelpUs was added

--- src/pageComponents/HomePage/blocks/OurAchievements/OurAchievements.tsx
+++ src/pageComponents/HomePage/blocks/OurAchievements/OurAchievements.tsx
@@ -1,8 +1,8 @@
-import { Button } from '~components/Buttons/Button';
 import { Container } from '~components/Container/Container';
 import { Section } from '~components/Section/Section';
 import { Text } from '~components/Text/Text';
 
+import { ButtonHelpUs } from '../../ButtonHelpUs/ButtonHelpUs';
 import { blocks } from './blocks';
 
 import s from './OurAchievements.module.scss';
@@ -23,7 +23,9 @@ export function OurAchievements() {
 									{line[1]}
 								</Text>
 								<div className={s.achievement}>
-									<Text variant="various1">{block.number}</Text>
+									<Text variant="various1" className={s.number}>
+										{block.number}
+									</Text>
 									<Text variant="p">{block.unit}</Text>
 								</div>
 							</div>
@@ -31,9 +33,9 @@ export function OurAchievements() {
 					})}
 				</div>
 				<Text variant="p">*Інформація подана станом на 01.07.2023</Text>
-				<Button type="secondary" className={s.button}>
-					Допомогти нам
-				</Button>
+				<div className={s.button}>
+					<ButtonHelpUs />
+				</div>
 			</Container>
 		</Section>
 	);
--- src/pageComponents/HomePage/blocks/OurAchievements/blocks.ts
+++ src/pageComponents/HomePage/blocks/OurAchievements/blocks.ts
@@ -6,7 +6,7 @@ interface Block {
 }
 
 export const blocks: Block[] = [
-	{ id: 1, title: 'Було видано наборів\nгуманітарної допомоги', number: '12 358', unit: '' },
+	{ id: 1, title: 'Було видано наборів\nгуманітарної допомоги', number: '12 358', unit: 'шт' },
 	{ id: 2, title: 'Було отримано\nгуманіарної допомоги', number: '264', unit: 'тонн' },
 	{ id: 3, title: 'Нам\nзадонатили', number: '104 228', unit: 'UAH' },
 ];
