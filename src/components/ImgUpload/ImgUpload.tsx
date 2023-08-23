b894e5cf0adb12f1cef582da77066af8a10446fc
Bittersweet-girl <enatav1@gmail.com>
2023-08-23 15:13:40 +0300

    feat: add functions to change image, clear image and display picked image

--- src/components/ImgUpload/ImgUpload.tsx
+++ src/components/ImgUpload/ImgUpload.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react';
+
 import Image from 'next/image';
 
 import { Icon } from '~components/Icon/Icon';
@@ -5,25 +7,50 @@ import { Icon } from '~components/Icon/Icon';
 import s from './ImgUpload.module.scss';
 
 export function ImgUpload() {
+	const [image, setImage] = useState<string>('');
+	const fileClick = useRef<HTMLInputElement>(null);
+
+	const changeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files) {
+			setImage(URL.createObjectURL(event.target.files[0]));
+		}
+	};
+
+	const handleClick = () => {
+		if (fileClick.current !== null) {
+			fileClick.current.click();
+		}
+	};
+
+	const clearFile = () => {
+		setImage('');
+	};
+
 	return (
 		<div className={s.ImgUpload}>
 			<div className={s.imgBlock}>
-				<Image
-					src='/svg/blank-img.svg'
-					alt='card-img'
-					width={100}
-					height={100}
-					className={s.img}
-				/>
+				{!image ?
+					<Image
+						priority={true}
+						src={'/svg/blank-img.svg'}
+						alt='card-img'
+						width={100}
+						height={100}
+						className={s.imgDefault}
+					/>
+					:
+					<Image
+						src={image}
+						alt='card-img'
+						width={712}
+						height={300}
+					/>}
 			</div>
 			<div className={s.iconBlock}>
-				<Icon icon="icon--upload" className={s.icon} /> {/*onClick={}*/}
-				<Icon icon="icon--trash" className={s.icon} />
+				<Icon icon="icon--upload" className={s.icon} onClick={handleClick} />
+				<Icon icon="icon--trash" className={s.icon} onClick={clearFile} />
 			</div>
-
-			{/* <label>
-				<input type='file' />
-			</label> */}
+			<input type='file' className={s.hidden} ref={fileClick} onChange={changeFile} accept='image/*, .png, .jpeg, .web' />
 		</div>
 	);
 }
