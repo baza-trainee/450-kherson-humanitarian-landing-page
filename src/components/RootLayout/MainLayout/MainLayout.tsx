b25017fa319605fde33d067d4c8e275237b71874
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2023-08-29 10:44:37 +0000

    Deploying to gh-pages from  @ 5ac2dcafd98b736cba901689aff5a046256869b5 🚀

--- src/components/RootLayout/MainLayout/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import s from './MainLayout.module.scss';
-
-interface MainLayoutProps {
-	children: React.ReactNode;
-}
-
-export function MainLayout({ children }: MainLayoutProps) {
-	return <main className={s.MainLayout}>{children}</main>;
-}
