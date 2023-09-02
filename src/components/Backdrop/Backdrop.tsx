ce5cfc215f9da4d82800b9aaa8155e69b47cebb9
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-02 12:34:51 +0300

    refactor: add Backdrop component

---
+++ src/components/Backdrop/Backdrop.tsx
@@ -0,0 +1,41 @@
+import { forwardRef, useEffect, useRef } from 'react';
+
+import clsx from 'clsx';
+
+import { mergeRefs } from '~components/helpers/mergeRefs';
+
+import s from './Backdrop.module.scss';
+
+export type BackdropElement = HTMLButtonElement;
+
+export interface BackdropProps extends ReactHTMLElementAttributes<BackdropElement> {
+	opacity?: number;
+	disabled?: boolean;
+	show: boolean;
+}
+
+export const Backdrop = forwardRef<BackdropElement, BackdropProps>(
+	({ className, disabled, show, opacity, ...rest }, ref) => {
+		const backdropRef = useRef<HTMLButtonElement>(null);
+
+		useEffect(() => {
+			if (backdropRef && opacity) backdropRef.current?.style.setProperty('--backdrop-opacity', `${opacity}%`);
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, [backdropRef]);
+
+		const componentClass = [disabled && s.disabled, show && s.show];
+
+		return (
+			<button
+				type="button"
+				className={clsx(s.Backdrop, className, componentClass)}
+				ref={mergeRefs([ref, backdropRef])}
+				{...rest}
+			>
+				{}
+			</button>
+		);
+	},
+);
+
+Backdrop.displayName = 'Backdrop';
