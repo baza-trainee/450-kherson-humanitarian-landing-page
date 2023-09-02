89398abc38119326c9bfd95919599cce717e0a15
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-02 15:39:46 +0300

    feat: add LoginPage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,14 @@
 import { LogInPage } from '~/pageComponents/LogInPage/LogInPage';
+import { LayoutLite } from '~components/LayoutLite/LayoutLite';
 import { Meta } from '~components/Meta/Meta';
 import { APP } from '~constants/APP';
 
 export default function LogIn() {
 	return (
-		<Meta title={APP.name}>
-			<LogInPage />
-		</Meta>
+		<LayoutLite>
+			<Meta title={APP.name}>
+				<LogInPage />
+			</Meta>
+		</LayoutLite>
 	);
 }
