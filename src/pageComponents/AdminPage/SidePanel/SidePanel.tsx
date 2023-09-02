fdf2026609e44b3e98fda019469513ac87c1c9b7
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-02 15:44:45 +0300

    feat: add logout

--- src/pageComponents/AdminPage/SidePanel/SidePanel.tsx
+++ src/pageComponents/AdminPage/SidePanel/SidePanel.tsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx';
+import Cookies from 'js-cookie';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { Icon } from '~components/Icon/Icon';
+import { ROUTES } from '~constants/ROUTES';
 
 import { navigationList } from './navigationList';
 import logo from '/public/svg/logo.svg';
@@ -11,21 +13,26 @@ import logo from '/public/svg/logo.svg';
 import s from './SidePanel.module.scss';
 
 export function SidePanel() {
-	const { query } = useRouter();
+	const router = useRouter();
 
-	const isActive = (slug: string) => query.slug === slug && s.active;
+	const isActive = (slug: string) => router.query.slug === slug && s.active;
+
+	const handleLogOutButtonOnClick = () => {
+		Cookies.remove('token');
+		router.push(ROUTES.login);
+	};
 
 	return (
 		<aside className={s.SidePanel}>
 			<div className={s.logo}>
-				<Image src={logo} alt="logo" width={142} height={60} />
+				<Image src={logo} alt="logo" width={200} height={80} />
 			</div>
 			<nav className={s.navigation}>
 				{navigationList.map((listItem) => (
 					<Link
 						key={listItem.slug}
 						href={{
-							pathname: '/admin/[slug]',
+							pathname: `${ROUTES.admin}/[slug]`,
 							query: { slug: listItem.slug },
 						}}
 						className={clsx(s.navigationItem, isActive(listItem.slug))}
@@ -34,17 +41,20 @@ export function SidePanel() {
 					</Link>
 				))}
 			</nav>
-			<button className={s.logout}>
-				<Icon
-					icon="icon--log-out"
-					colors={{
-						default: 'var(--color--warning-1)',
-						hover: 'var(--color--primary-1)',
-						click: 'var(--color--warning-1)',
+			<div className={s.bottom}>
+				<Link
+					href={{
+						pathname: `${ROUTES.admin}/change-password`,
 					}}
-				/>
-				Вийти
-			</button>
+					className={clsx(s.navigationItem, isActive('change-password'))}
+				>
+					Змінити пароль
+				</Link>
+				<button className={s.logout} onClick={handleLogOutButtonOnClick}>
+					<Icon icon="icon--log-out" className={s.logoutIcon} />
+					Вийти
+				</button>
+			</div>
 		</aside>
 	);
 }
