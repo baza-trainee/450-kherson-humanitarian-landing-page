116f6285e3b6a0a66aefa12831fd7d6713960932
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-09-06 07:33:37 +0300

    feat: add auto switch on slider

--- src/pageComponents/HomePage/blocks/Hero/Hero.tsx
+++ src/pageComponents/HomePage/blocks/Hero/Hero.tsx
@@ -20,15 +20,48 @@ import s from './Hero.module.scss';
 export function Hero() {
 	const [currentSlide, setCurrentSlide] = useState(0);
 	const [loaded, setLoaded] = useState(false);
-	const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-		initial: 0,
-		slideChanged(slider) {
-			setCurrentSlide(slider.track.details.rel);
+	const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
+		{
+			initial: 0,
+			slideChanged(slider) {
+				setCurrentSlide(slider.track.details.rel);
+			},
+			created() {
+				setLoaded(true);
+			},
+			loop: true,
 		},
-		created() {
-			setLoaded(true);
-		},
-	});
+		[
+			(slider) => {
+				let timeout: ReturnType<typeof setTimeout>;
+				let mouseOver = false;
+				function clearNextTimeout() {
+					clearTimeout(timeout);
+				}
+				function nextTimeout() {
+					clearTimeout(timeout);
+					if (mouseOver) return;
+					timeout = setTimeout(() => {
+						slider.next();
+					}, 3000);
+				}
+				slider.on('created', () => {
+					slider.container.addEventListener('mouseover', () => {
+						mouseOver = true;
+						clearNextTimeout();
+					});
+					slider.container.addEventListener('mouseout', () => {
+						mouseOver = false;
+						nextTimeout();
+					});
+					nextTimeout();
+				});
+				slider.on('dragStarted', clearNextTimeout);
+				slider.on('animationEnded', nextTimeout);
+				slider.on('updated', nextTimeout);
+			},
+		],
+	);
 
 	const { isScreenTabletSm } = useScreenQuery();
 
