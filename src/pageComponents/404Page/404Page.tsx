a00e5eb74151876dbff45bd678e2bbcad976bd90
Andrey <luchko.andrey@gmail.com>
2023-09-18 11:34:25 +0300

    refactor: change container and sort styles

--- src/pageComponents/404Page/404Page.tsx
+++ src/pageComponents/404Page/404Page.tsx
@@ -12,26 +12,24 @@ export function Page404() {
 	return (
 		<Section className={s.Page404}>
 			<Container className={s.container}>
-				<div className={s.wrapper}>
-					<Text variant="h2" className={s.title}>
-						Сторінка не знайдена
-					</Text>
-					<div className={s.content}>
-						<div className={s.image}>
-							<Image src={errorImage} alt="error massage" fill priority />
-						</div>
-						<div>
-							<Text variant="h3" className={s.text}>
-								Шкода, але сторінку не знайдено.
-							</Text>
-							<Text variant="h3" className={s.text}>
-								Можливо, її перенесли або видалили.
-							</Text>
-						</div>
-						<ButtonLink href="/" className={s.button}>
-							На головну
-						</ButtonLink>
+				<Text variant="h2" className={s.title}>
+					Сторінка не знайдена
+				</Text>
+				<div className={s.content}>
+					<div className={s.image}>
+						<Image src={errorImage} alt="error massage" fill priority />
 					</div>
+					<div>
+						<Text variant="h3" className={s.text}>
+							Шкода, але сторінку не знайдено.
+						</Text>
+						<Text variant="h3" className={s.text}>
+							Можливо, її перенесли або видалили.
+						</Text>
+					</div>
+					<ButtonLink href="/" className={s.button}>
+						На головну
+					</ButtonLink>
 				</div>
 			</Container>
 		</Section>
--- src/pageComponents/NotificationPage/SuccessRegistrationPage/SuccessRegistrationPage.tsx
+++ src/pageComponents/NotificationPage/SuccessRegistrationPage/SuccessRegistrationPage.tsx
@@ -1,11 +1,9 @@
-import { ButtonLink } from '~components/Buttons/ButtonLink';
-import { Container } from '~components/Container/Container';
-import { Section } from '~components/Section/Section';
 import { Text } from '~components/Text/Text';
 
 import s from './SuccessRegistrationPage.module.scss';
 
 interface ParsedUrlQuery {
+	// slug: string;
 	issueDate?: string;
 	issueTime?: string;
 }
@@ -17,26 +15,17 @@ interface SuccessRegistrationPageProps {
 export function SuccessRegistrationPage(props: SuccessRegistrationPageProps) {
 	const { issueDate, issueTime } = props.data;
 	return (
-		<Section>
-			<Container className={s.container}>
-				<div className={s.wrapper}>
-					<div className={s.text}>
-						<Text variant="h2" className={s.title}>
-							Вітаємо!
-						</Text>
-						<Text variant="h3" className={s.subTitle}>
-							Ви успішно зареєструвались на отримання гуманітарної допомоги
-						</Text>
-						<Text variant="p" className={s.description}>
-							{`Видача допомоги відбудеться ${issueDate} о ${issueTime} годині за адресою: м. Кривий Ріг, вул.
+		<>
+			<Text variant="h2" className={s.title}>
+				Вітаємо!
+			</Text>
+			<Text variant="h3" className={s.subTitle}>
+				Ви успішно зареєструвались на отримання гуманітарної допомоги
+			</Text>
+			<Text variant="p" className={s.description}>
+				{`Видача допомоги відбудеться ${issueDate} о ${issueTime} годині за адресою: м. Кривий Ріг, вул.
 							Гетьманська 39А.`}
-						</Text>
-					</div>
-					<ButtonLink href="/" className={s.button}>
-						На головну
-					</ButtonLink>
-				</div>
-			</Container>
-		</Section>
+			</Text>
+		</>
 	);
 }
