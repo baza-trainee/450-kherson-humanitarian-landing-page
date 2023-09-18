ec8e62ac32f5d35158a6190c03e545f7e9ade640
Andrey <luchko.andrey@gmail.com>
2023-09-18 11:41:02 +0300

    refactor: add getserverSideProps for query

--- src/pages/notification/[slug]/index.tsx
+++ src/pages/notification/[slug]/index.tsx
@@ -1,14 +1,26 @@
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next/types';
+
 import { NotificationPage } from '~/pageComponents/NotificationPage/NotificationPage';
 import { LayoutLite } from '~components/LayoutLite/LayoutLite';
 import { Meta } from '~components/Meta/Meta';
 import { APP } from '~constants/APP';
 
-export default function Notification() {
+export default function Notification(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return (
 		<LayoutLite>
 			<Meta title={APP.name}>
-				<NotificationPage />
+				<NotificationPage queryData={props.queryData} />
 			</Meta>
 		</LayoutLite>
 	);
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	const { query } = context;
+
+	return {
+		props: {
+			queryData: query,
+		},
+	};
 }
