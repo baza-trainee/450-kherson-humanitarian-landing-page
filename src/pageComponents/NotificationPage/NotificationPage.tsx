0e49fd16d73e106c614f8f22a3fcd2292d5dac69
Andrey <luchko.andrey@gmail.com>
2023-09-18 11:39:36 +0300

    refactor: change method of connection of component and add switch to Page404 in case wrong path

--- src/pageComponents/NotificationPage/NotificationPage.tsx
+++ src/pageComponents/NotificationPage/NotificationPage.tsx
@@ -1,17 +1,45 @@
-import { useRouter } from 'next/router';
+import type { ParsedUrlQuery } from 'querystring';
 
+import { ButtonLink } from '~components/Buttons/ButtonLink';
+import { Container } from '~components/Container/Container';
+import { Section } from '~components/Section/Section';
+
+import { Page404 } from '../404Page/404Page';
 import { SuccessRegistrationPage } from './SuccessRegistrationPage/SuccessRegistrationPage';
 import { UnsuccessRegistrationPage } from './UnsuccessRegistrationPage/UnsuccessRegistrationPage';
 
 import s from './NotificationPage.module.scss';
 
-export function NotificationPage() {
-	const { query } = useRouter();
+const pages = ['unsuccess-registration', 'success-registration'];
+
+interface NotificationPageProps {
+	queryData: ParsedUrlQuery;
+}
+
+export function NotificationPage({ queryData }: NotificationPageProps) {
+	const slug = typeof queryData.slug === 'string' ? queryData.slug : queryData.slug?.[0];
 
 	return (
-		<main className={s.NotificationPage}>
-			{query.slug === 'unsuccess-registration' && <UnsuccessRegistrationPage />}
-			{query.slug === 'success-registration' && <SuccessRegistrationPage data={query} />}
-		</main>
+		<>
+			{slug && pages.includes(slug) ? (
+				<Section>
+					<Container className={s.container}>
+						<div className={s.content}>
+							{
+								{
+									'unsuccess-registration': <UnsuccessRegistrationPage />,
+									'success-registration': <SuccessRegistrationPage data={queryData} />,
+								}[slug]
+							}
+							<ButtonLink href="/" className={s.button}>
+								На головну
+							</ButtonLink>
+						</div>
+					</Container>
+				</Section>
+			) : (
+				<Page404 />
+			)}
+		</>
 	);
 }
