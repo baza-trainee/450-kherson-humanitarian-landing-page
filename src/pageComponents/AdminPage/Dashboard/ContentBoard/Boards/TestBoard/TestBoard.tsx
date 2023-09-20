ab7d3087df74fe0fdd44716a863b667f849b529e
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-20 20:48:55 +0300

    refactor: remove cleanup TestBoard component

--- src/pageComponents/AdminPage/Dashboard/ContentBoard/Boards/TestBoard/TestBoard.tsx
+++ src/pageComponents/AdminPage/Dashboard/ContentBoard/Boards/TestBoard/TestBoard.tsx
@@ -1,15 +1,6 @@
-import { ImgUpload } from '~components/ImgUpload/ImgUpload';
-
-interface TestBoardProps {
-	tabId: string;
-	tabName: string;
-}
-
-export function TestBoard({ tabId, tabName }: TestBoardProps) {
+export function TestBoard() {
 	return (
 		<>
-			<ImgUpload />
-			<p>ContentBoard</p>
 			<p>ContentBoard</p>
 		</>
 	);
