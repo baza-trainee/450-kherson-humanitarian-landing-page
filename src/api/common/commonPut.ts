a4dc9c214eac8fc917ea0a436d6cb07595656cd9
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-21 23:29:51 +0300

    refactor: remove ApiResponse type from common api

--- src/api/common/commonPut.ts
+++ src/api/common/commonPut.ts
@@ -2,11 +2,6 @@ import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
 import { requestWrapper } from '~api/helpers/requestWrapper';
-import type { ApiResponse } from '~api/types/backend/responses/ApiResponse';
 
-export const clientPut = <R, B>(
-	endpoint: string,
-	body: B,
-	requestConfig?: AxiosRequestConfig,
-): Promise<ApiResponse<R>> =>
+export const clientPut = <R, B>(endpoint: string, body: B, requestConfig?: AxiosRequestConfig) =>
 	requestWrapper((apiUrl: string) => axios.put<R>(`${apiUrl}/${endpoint}`, body, requestConfig));
