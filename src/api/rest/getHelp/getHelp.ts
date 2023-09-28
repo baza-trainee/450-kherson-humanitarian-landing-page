34f7ec22456c9f5bdcc9f5e45a1e82b683174f7b
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-09-28 13:28:55 +0300

    refactor: add getHelp api

---
+++ src/api/rest/getHelp/getHelp.ts
@@ -0,0 +1,15 @@
+import { getSrcUrlFromTag } from '~/pageComponents/HomePage/blocks/GetHelp/Info/helpers/getSrcUrlFromTag';
+
+const mapIframe =
+	'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2674.50337692512!2d33.37251627643731!3d47.9072967671648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40db20a8af29a41f%3A0x38ba7c4ae3e4aaf1!2z0LLRg9C70LjRhtGPINCT0LXRgtGM0LzQsNC90YHRjNC60LAsIDM50LAsINCa0YDQuNCy0LjQuSDQoNGW0LMsINCU0L3RltC_0YDQvtC_0LXRgtGA0L7QstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1MDAwNw!5e0!3m2!1suk!2sua!4v1695717933599!5m2!1suk!2sua" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+//* backend emulation
+export const getInfo = () => {
+	const mapUrl = getSrcUrlFromTag(mapIframe);
+	return {
+		data: {
+			address: 'м. Кривий Ріг, вул. Гетьманська 39А',
+			mapUrl: mapUrl || '',
+		},
+	};
+};
--- src/pageComponents/HomePage/blocks/GetHelp/GetHelp.tsx
+++ src/pageComponents/HomePage/blocks/GetHelp/GetHelp.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import type { GetHelpInfo } from '~api/types/GetHelp/GetHelpInfo';
 import type { GetHelpLists } from '~api/types/GetHelp/GetHelpLists';
 import type { HelpCategories } from '~api/types/GetHelp/HelpCategories';
 import { Container } from '~components/Container/Container';
@@ -14,10 +15,12 @@ import s from './GetHelp.module.scss';
 
 interface GetHelp {
 	lists?: GetHelpLists;
+	info?: GetHelpInfo;
 }
 
-export function GetHelp({ lists }: GetHelp) {
+export function GetHelp({ lists, info }: GetHelp) {
 	const [activeTab, setActiveTab] = useState<HelpCategories>('idp');
+
 	return (
 		<Section className={s.GetHelp} id="get-help">
 			<Container className={s.container}>
@@ -25,14 +28,19 @@ export function GetHelp({ lists }: GetHelp) {
 					<div className={s.headingContainer}>
 						<Text variant="h2">Отримати допомогу</Text>
 						<div className={s.description}>
-							<Text variant="h3">Для отримання гуманітарної допомоги, заповніть, будь ласка, форму нижче</Text>
+							<Text variant="h3">
+								Для отримання гуманітарної допомоги, заповніть, будь ласка, форму нижче
+							</Text>
 							<div className={s.alert}>
-								<Icon icon="icon--attention" colors={{ default: 'var(--color--warning-1)' }} />
+								<Icon
+									icon="icon--attention"
+									colors={{ default: 'var(--color--warning-1)' }}
+								/>
 								<Text variant="subtitle">
 									<div className="icon-24--attention" />
-									Видача гуманітарної допомоги відбувається тільки для жителів Кривого Рогу та за наявністю
-									документів, зазначених у формі реєстрації, а також документів інших членів сім&apos;ї, що
-									підтверджують їхню особу.
+									Видача гуманітарної допомоги відбувається тільки для жителів Кривого Рогу
+									та за наявністю документів, зазначених у формі реєстрації, а також
+									документів інших членів сім&apos;ї, що підтверджують їхню особу.
 									<br />
 									<br />
 									Набори видаються з розрахунку до 4 людей на 1 набір.
@@ -42,7 +50,7 @@ export function GetHelp({ lists }: GetHelp) {
 					</div>
 					<Form lists={lists} setActiveTab={setActiveTab} />
 				</div>
-				<Info lists={lists} activeTab={activeTab} />
+				<Info lists={lists} activeTab={activeTab} info={info} />
 			</Container>
 		</Section>
 	);
