29f1cb8713741887f1bf1890ef97ae3c6b89a767
Andrey <luchko.andrey@gmail.com>
2023-09-30 16:57:33 +0300

    refactor: remove redundant module style

--- src/pageComponents/NotificationPage/SuccessRegistrationPage/SuccessRegistrationPage.tsx
+++ src/pageComponents/NotificationPage/SuccessRegistrationPage/SuccessRegistrationPage.tsx
@@ -1,7 +1,5 @@
 import { Text } from '~components/Text/Text';
 
-import s from './SuccessRegistrationPage.module.scss';
-
 interface ParsedUrlQuery {
 	issueDate?: string;
 	issueTime?: string;
@@ -15,15 +13,10 @@ export function SuccessRegistrationPage(props: SuccessRegistrationPageProps) {
 	const { issueDate, issueTime } = props.data;
 	return (
 		<>
-			<Text variant="h2" className={s.title}>
-				Вітаємо!
-			</Text>
-			<Text variant="h3" className={s.subTitle}>
-				Ви успішно зареєструвались на отримання гуманітарної допомоги
-			</Text>
-			<Text variant="p" className={s.description}>
-				{`Видача допомоги відбудеться ${issueDate} о ${issueTime} годині за адресою: м. Кривий Ріг, вул.
-							Гетьманська 39А.`}
+			<Text variant="h2">Вітаємо!</Text>
+			<Text variant="h3">Ви успішно зареєструвались на отримання гуманітарної допомоги</Text>
+			<Text variant="p">
+				{`Видача допомоги відбудеться ${issueDate} о ${issueTime} годині за адресою: м. Кривий Ріг, вул.Гетьманська 39А.`}
 			</Text>
 		</>
 	);
