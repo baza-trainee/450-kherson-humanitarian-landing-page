cad28087e1595318e02f228123854689630a4f3e
Andrey <luchko.andrey@gmail.com>
2023-09-30 16:58:21 +0300

    refactor: fix errors text

--- src/pageComponents/NotificationPage/UnsuccessRegistrationPage/UnsuccessRegistrationPage.tsx
+++ src/pageComponents/NotificationPage/UnsuccessRegistrationPage/UnsuccessRegistrationPage.tsx
@@ -22,9 +22,8 @@ export function UnsuccessRegistrationPage(props: UnsuccessRegistrationPageProps)
 			const message = getErrorMessageFromCode(error, {
 				432: 'Список не активний',
 				434: 'Реєстрація закрита або список заповнений',
-				437: 'Не вдалося зареєструвати. Спробуйте пізніше',
-				438: 'Ваш e-mail вже зареєстрований',
-				500: 'Спробуйте пізніше',
+				437: 'Не вдалося зареєструвати. Спробуйте ще раз',
+				438: 'Ви вже зареєстровані',
 			});
 			setErrorMessage(message);
 		}
@@ -33,9 +32,9 @@ export function UnsuccessRegistrationPage(props: UnsuccessRegistrationPageProps)
 	return (
 		<>
 			<Text variant="h2" className={s.title}>
-				Увага!
+				Помилка реєстрації!
 			</Text>
-			<Text variant="h3" className={s.subTitle}>
+			<Text variant="p" className={s.subTitle}>
 				{errorMessage}
 			</Text>
 		</>
