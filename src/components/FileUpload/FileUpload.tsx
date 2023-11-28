ab41d5a3d628ccc66c9139b51b3ec8af16619392
Bittersweet-girl <enatav1@gmail.com>
2023-11-28 23:06:57 +0200

    fix: change component to new design version

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,64 +1,68 @@
-import { forwardRef, useEffect } from 'react';
-import { type FieldErrors, type FieldValues, useForm } from 'react-hook-form';
+import { forwardRef, useEffect, useState } from 'react';
+import { type FieldErrors, type FieldValues } from 'react-hook-form';
 
 import { Icon } from '~components/Icon/Icon';
-import { TextInput } from '~components/inputs/TextInput/TextInput';
+import { InputWrapper } from '~components/inputs/InputWrapper/InputWrapper';
+import { Text } from '~components/Text/Text';
+import { BASE_URL } from '~constants/BASE_URL';
 
 import s from './FileUpload.module.scss';
 
 export type FileUploadElement = HTMLInputElement;
 
 interface FileUploadProps {
-	fileRegister?: FieldValues;
-	label: string;
+	register?: FieldValues;
 	watch?: (name: string) => FieldValues;
 	errors?: FieldErrors<FieldValues>;
-}
-interface FormFields {
-	textRegister: string;
+	deleteFile: () => void;
+	label: string;
 }
 
 export const FileUpload = forwardRef<FileUploadElement, FileUploadProps>(
-	({ fileRegister, label, watch, errors }, ref) => {
-		const file = watch ? watch(fileRegister ? fileRegister.name : null) : null;
-
-		const { register, setValue } = useForm<FormFields>({
-			mode: 'onChange',
-		});
-
-		const textRegister = register('textRegister');
+	({ register, watch, errors, deleteFile, label }, ref) => {
+		const file = watch ? watch(register ? register.name : null) : null;
+		const [fileName, setFileName] = useState<string>('');
 
 		useEffect(() => {
 			if (typeof file === 'string') {
-				const path = file as string;
-				setValue('textRegister', path.slice(path.lastIndexOf('/') + 1, path.length));
+				const path = process.env.NODE_ENV === 'development' ? `${BASE_URL}${file}` : file;
+				setFileName(path.slice(path.lastIndexOf('/') + 1, path.length));
 			} else if (file && file.length > 0) {
-				setValue('textRegister', file[0].name);
+				setFileName(file[0].name);
 			}
 			// eslint-disable-next-line react-hooks/exhaustive-deps
 		}, [file]);
 
+		const handleShowFile = () => {
+			if (typeof file === 'string') {
+				window.open(`${BASE_URL}${file}`);
+			} else if (file && file.length > 0) {
+				window.open(URL.createObjectURL(file[0]));
+			}
+		};
+
 		return (
-			<div className={s.fileUploadBlock}>
-				<TextInput
-					className={s.fileUpload}
-					required
-					label={label}
-					maxLength={0}
-					errors={errors}
-					register={textRegister}
-				/>
-				<label>
-					<Icon icon="icon--upload" className={s.icon} />
-					<input
-						type="file"
-						className={s.hidden}
-						ref={ref}
-						accept="application/x-7z-compressed, application/vnd.rar, application/zip, application/gzip, application/x-bzip, application/x-bzip2, application/zip, text/plain, text/markdown, application/rtf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/epub+zip, application/pdf"
-						{...fileRegister}
-					/>
-				</label>
-			</div>
+			<InputWrapper label={label} className={s.borderWrapper}>
+				<div className={s.fileUploadBlock}>
+					<Text variant="footer" className={s.underline}>
+						{fileName}
+					</Text>
+					<div className={s.iconBlock}>
+						<label>
+							<Icon icon="icon--upload" className={s.icon} />
+							<input
+								type="file"
+								className={s.hidden}
+								ref={ref}
+								accept="application/x-7z-compressed, application/vnd.rar, application/zip, application/gzip, application/x-bzip, application/x-bzip2, application/zip, text/plain, text/markdown, application/rtf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/epub+zip, application/pdf"
+								{...register}
+							/>
+						</label>
+						<Icon icon="icon--eye" className={s.icon} onClick={handleShowFile} />
+						<Icon icon="icon--trash" className={s.icon} onClick={deleteFile} />
+					</div>
+				</div>
+			</InputWrapper>
 		);
 	},
 );
