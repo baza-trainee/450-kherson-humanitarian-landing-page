47634dc26218887094b85ae1e0bfb67335eeefe0
Vadim-27 <vadim.bulanyi@gmail.com>
2023-12-01 11:32:55 +0100

    refactor: change default values

--- src/api/rest/ourAchievements/dto/transformOurAchievementsDTO.ts
+++ src/api/rest/ourAchievements/dto/transformOurAchievementsDTO.ts
@@ -2,9 +2,9 @@ import type { OurAchievementsResponse } from '~api/types/backend/responses/OurAc
 
 export function transformOurAchievementsDTO(data: OurAchievementsResponse) {
 	return {
-		issuedSets: data.issuedHumanitarianKits?.toString() || '0',
-		receivedHelp: data.receivedHumanitarianAid?.toString() || '0',
-		donations: data.sumDonats?.toString() || '0',
+		issuedSets: data.issuedHumanitarianKits?.toString() || '',
+		receivedHelp: data.receivedHumanitarianAid?.toString() || '',
+		donations: data.sumDonats?.toString() || '',
 		issueDate: data.infoAtDate ? new Date(data.infoAtDate).toLocaleDateString() : '',
 	};
 }
