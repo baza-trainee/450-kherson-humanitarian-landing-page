d7d833549f0a7be0e5a4bb5bc91a0ec533287e9b
Vadim-27 <vadim.bulanyi@gmail.com>
2023-12-01 11:25:44 +0100

    refactor: add functions to state

--- src/pageComponents/AdminPage/store/useOurAchievementsBoardState.ts
+++ src/pageComponents/AdminPage/store/useOurAchievementsBoardState.ts
@@ -1,21 +1,31 @@
 import { create } from 'zustand';
 
 import { api } from '~api/index';
+import type { OurAchievementsRequest } from '~api/types/backend/requests/OurAchievementsRequest';
 import type { ErrorResponse } from '~api/types/backend/responses/ErrorResponse';
 import type { OurAchievements } from '~api/types/ourAchievements/OurAchievements';
 import { returnAppError } from '~helpers/returnAppError';
 
 interface UseOurAchievementsBoardState {
+	isModalOnSuccessSaveOpen: boolean;
 	isLoading: boolean;
 	error: ErrorResponse | null;
 	ourAchievementsBoardData: OurAchievements | null;
-	getBoardData: (name: string) => Promise<void>;
+
+	getBoardData: () => Promise<void>;
+	setIsModalOnSuccessSaveClose: () => void;
+	updateOurAchievementsDataBoard: (body: OurAchievementsRequest)=>Promise<void>;
 }
 
 export const useOurAchievementsBoardState = create<UseOurAchievementsBoardState>((set) => ({
+	isModalOnSuccessSaveOpen: false,
 	isLoading: false,
 	error: null,
 	ourAchievementsBoardData: null,
+
+	setIsModalOnSuccessSaveClose: () => {
+		set({ isModalOnSuccessSaveOpen: false });
+	},
 
 	getBoardData: async () => {
 		set({ isLoading: true });
@@ -23,9 +33,10 @@ export const useOurAchievementsBoardState = create<UseOurAchievementsBoardState>
 
 		try {
 			const resp = await api.ourAchievements.getOurAchievements();
-			if ('data' in resp) set({ ourAchievementsBoardData: resp.data });
+			if ('data' in resp) {
+				set({ ourAchievementsBoardData: resp.data });
 
-
+			}
 			if (resp && 'error' in resp) {
 				set({ error: resp.error });
 			}
@@ -35,4 +46,21 @@ export const useOurAchievementsBoardState = create<UseOurAchievementsBoardState>
 			set({ isLoading: false });
 		}
 	},
+	updateOurAchievementsDataBoard: async (body) => {
+		set({ isLoading: true });
+		set({ error: null });
+		try {
+			const resp = await api.ourAchievements.putOurAchievements(body);
+			if ('data' in resp) {
+
+				set({ isModalOnSuccessSaveOpen: true });
+			} else set({ error: resp.error });
+		} catch (error) {
+			set({ error: returnAppError(error) });
+		} finally {
+			set({ isLoading: false });
+		}
+	},
 }));
+
+
