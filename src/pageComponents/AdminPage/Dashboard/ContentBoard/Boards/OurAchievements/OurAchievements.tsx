a0c87b88cc38732332d55c6fff72e53e29f26a00
Vadim-27 <vadim.bulanyi@gmail.com>
2023-12-01 11:31:35 +0100

    refactor: add show modals success and error,  add loader,  add pattern

--- src/pageComponents/AdminPage/Dashboard/ContentBoard/Boards/OurAchievements/OurAchievements.tsx
+++ src/pageComponents/AdminPage/Dashboard/ContentBoard/Boards/OurAchievements/OurAchievements.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 
 import ActionButtons from '~/pageComponents/AdminPage/components/ActionButtons/ActionButtons';
 import { useOurAchievementsBoardState } from '~/pageComponents/AdminPage/store/useOurAchievementsBoardState';
-import { api } from '~api/index';
+import { useTabsState } from '~/pageComponents/AdminPage/store/useTabsState';
+import { Button } from '~components/Buttons/Button';
 import { TextInputWithCounter } from '~components/inputs/TextInput/TextInputWithCounter';
+import { Loader } from '~components/Loader/Loader';
+import { ModalPop } from '~components/ModalPop/ModalPop';
 
 import s from './ourAchievements.module.scss';
 
@@ -16,13 +19,30 @@ export interface FormFields {
 	issueDate: string;
 }
 export function OurAchievements() {
-	const { ourAchievementsBoardData, getBoardData } = useOurAchievementsBoardState((state) => ({
+	const { isTabsClickBlocked, setIsTabsClickBlocked } = useTabsState((state) => ({
+		isTabsClickBlocked: state.isTabsClickBlocked,
+		setIsTabsClickBlocked: state.setIsTabsClickBlocked,
+	}));
+	const {
+		ourAchievementsBoardData,
+		getBoardData,
+		isLoading,
+		isModalOnSuccessSaveOpen,
+		setIsModalOnSuccessSaveClose,
+		updateOurAchievementsDataBoard,
+		stateError,
+	} = useOurAchievementsBoardState((state) => ({
+		stateError: state.error,
+		isLoading: state.isLoading,
+		updateOurAchievementsDataBoard: state.updateOurAchievementsDataBoard,
+		isModalOnSuccessSaveOpen: state.isModalOnSuccessSaveOpen,
+		setIsModalOnSuccessSaveClose: state.setIsModalOnSuccessSaveClose,
 		ourAchievementsBoardData: state.ourAchievementsBoardData,
 		getBoardData: state.getBoardData,
 	}));
 
 	useEffect(() => {
-		getBoardData('achievements');
+		getBoardData();
 	}, [getBoardData]);
 
 	const {
@@ -30,11 +50,13 @@ export function OurAchievements() {
 		setValue,
 		handleSubmit,
 		reset,
+		watch,
+		clearErrors,
 		formState: { errors, isValid },
 	} = useForm<FormFields>({
 		mode: 'onSubmit',
 	});
-
+	const [errorMessage, setErrorMessage] = useState('');
 	const currentFormattedDate = () => {
 		return ourAchievementsBoardData
 			? ourAchievementsBoardData.issueDate
@@ -54,24 +76,31 @@ export function OurAchievements() {
 			setValue('donations', ourAchievementsBoardData.donations);
 			setValue('issueDate', isoDate);
 		}
+		setErrorMessage('');
+		clearErrors();
+		setIsTabsClickBlocked(false);
+
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ourAchievementsBoardData, setValue]);
+	}, [ourAchievementsBoardData]);
 
 	const registers = {
 		issuedSets: register('issuedSets', {
 			required: 'Поле не може бути пустим',
 			min: { value: 1, message: 'щось тут не так' },
 			max: { value: 9999999999, message: 'щось тут не так' },
+			pattern: { value: /^\d+$/, message: 'Введіть тільки цифри' },
 		}),
 		receivedHelp: register('receivedHelp', {
 			required: 'Поле не може бути пустим',
 			min: { value: 1, message: 'щось тут не так' },
 			max: { value: 9999999999, message: 'щось тут не так' },
+			pattern: { value: /^\d+$/, message: 'Введіть тільки цифри' },
 		}),
 		donations: register('donations', {
 			required: 'Поле не може бути пустим',
 			min: { value: 1, message: 'щось тут не так' },
 			max: { value: 9999999999, message: 'щось тут не так' },
+			pattern: { value: /^\d+$/, message: 'Введіть тільки цифри' },
 		}),
 		issueDate: register('issueDate', {
 			required: 'Поле не може бути пустим',
@@ -89,6 +118,28 @@ export function OurAchievements() {
 			issueDate: isoDate,
 		});
 	};
+	useEffect(() => {
+		const isoDate = currentFormattedDate();
+		watch((value) => {
+			if (
+				value.issuedSets !== ourAchievementsBoardData?.issuedSets ||
+				value.receivedHelp !== ourAchievementsBoardData?.receivedHelp ||
+				value.donations !== ourAchievementsBoardData?.donations ||
+				value.issueDate !== isoDate
+			) {
+				setIsTabsClickBlocked(true);
+			} else setIsTabsClickBlocked(false);
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [watch, ourAchievementsBoardData]);
+	useEffect(() => {
+		//*set message to show in Modal Error
+		if (stateError) {
+			if (stateError.status === 406)
+				setErrorMessage('Не правильно введені дані. Можливо є зайві символи');
+			if (stateError.status === 500) setErrorMessage(stateError.message);
+		}
+	}, [stateError]);
 
 	const onSubmit: SubmitHandler<FormFields> = async (data: FormFields) => {
 		const date = new Date(data.issueDate);
@@ -109,56 +160,87 @@ export function OurAchievements() {
 		};
 
 		if (data) {
-			await api.ourAchievements.putOurAchievements(body);
+			await updateOurAchievementsDataBoard(body);
 		}
+		setIsTabsClickBlocked(false);
 	};
 	return (
-		<form className={s.form}>
-			<TextInputWithCounter
-				type="text"
-				register={registers.issuedSets}
-				required
-				errors={errors}
-				label="Було видано гуманітарних наборів"
-				placeholder="200"
-				maxLength={12}
-				showInfo
-			/>
-			<TextInputWithCounter
-				type="text"
-				register={registers.receivedHelp}
-				required
-				errors={errors}
-				label="Було отримано гуманітарної допомоги"
-				placeholder="200"
-				maxLength={12}
-				showInfo
-			/>
-			<TextInputWithCounter
-				type="text"
-				register={registers.donations}
-				required
-				errors={errors}
-				label="Нам задонатили"
-				placeholder="200"
-				maxLength={12}
-				showInfo
-			/>
-			<TextInputWithCounter
-				type="date"
-				register={registers.issueDate}
-				required
-				errors={errors}
-				label="Інформація подана станом на"
-				placeholder="24.08.2091"
-				maxLength={10}
-				showInfo
-			/>
-			<ActionButtons
-				onSave={handleSubmit(onSubmit)}
-				onReset={handleUndoChanges}
-				isDataValid={isValid}
-			/>
-		</form>
+		<>
+			{(isLoading || !ourAchievementsBoardData) && <Loader />}
+			{!isLoading && ourAchievementsBoardData && (
+				<form className={s.form}>
+					<TextInputWithCounter
+						type="text"
+						register={registers.issuedSets}
+						required
+						errors={errors}
+						label="Було видано гуманітарних наборів"
+						placeholder="200"
+						maxLength={12}
+						showInfo
+					/>
+					<TextInputWithCounter
+						type="text"
+						register={registers.receivedHelp}
+						required
+						errors={errors}
+						label="Було отримано гуманітарної допомоги"
+						placeholder="200"
+						maxLength={12}
+						showInfo
+					/>
+					<TextInputWithCounter
+						type="text"
+						register={registers.donations}
+						required
+						errors={errors}
+						label="Нам задонатили"
+						placeholder="200"
+						maxLength={12}
+						showInfo
+					/>
+					<TextInputWithCounter
+						type="date"
+						register={registers.issueDate}
+						required
+						errors={errors}
+						label="Інформація подана станом на"
+						placeholder="24.08.2091"
+						maxLength={10}
+						showInfo
+					/>
+					<ActionButtons
+						onSave={handleSubmit(onSubmit)}
+						onReset={handleUndoChanges}
+						isDataValid={isValid}
+						isDisabled={!isTabsClickBlocked}
+					/>
+					{
+						isModalOnSuccessSaveOpen && (
+							<ModalPop
+								isOpen={isModalOnSuccessSaveOpen}
+								onClose={setIsModalOnSuccessSaveClose}
+								title="Вітаємо!"
+								leftButton={() => (
+									<Button onClick={setIsModalOnSuccessSaveClose}>Ок</Button>
+								)}
+							>
+								Ваші дані успішно збережено
+							</ModalPop>
+						) //add modal on success saving data on server
+					}
+					{errorMessage && (
+						<ModalPop
+							type="error"
+							title="Помилка при збереженні!"
+							isOpen={!!errorMessage}
+							onClose={() => setErrorMessage('')}
+						>
+							{errorMessage}
+						</ModalPop>
+					)}
+				</form>
+			)}
+		</>
 	);
 }
