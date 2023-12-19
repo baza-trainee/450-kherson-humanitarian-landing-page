b44fa4bcd0b061a62902da9701dfade038ea2699
Vadim-27 <vadim.bulanyi@gmail.com>
2023-12-19 10:49:09 +0100

    refactor: add getHelpAdmin

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import * as aboutUs from '~api/rest/aboutUs/aboutUs';
 import * as donations from '~api/rest/donations/donations';
 import * as exportList from '~api/rest/exportList/exportList';
 import * as getHelp from '~api/rest/getHelp/getHelp';
+import * as getHelpAdmin from '~api/rest/getHelp/getHelpAdmin';
 import * as hero from '~api/rest/hero/hero';
 import * as lists from '~api/rest/lists/lists';
 import * as ourAchievements from '~api/rest/ourAchievements/ourAchievements';
@@ -13,6 +14,7 @@ export const api = {
 	aboutUs,
 	exportList,
 	getHelp,
+	getHelpAdmin,
 	lists,
 	persons,
 	ourAchievements,
