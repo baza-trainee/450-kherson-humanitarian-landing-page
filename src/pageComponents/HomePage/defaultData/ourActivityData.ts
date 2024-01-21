474a7ac2417ca2d594e496dcd161db77b8d0aa0b
Bittersweet-girl <enatav1@gmail.com>
2024-01-21 19:27:02 +0200

    fix: change src and id to display data

--- src/pageComponents/HomePage/defaultData/ourActivityData.ts
+++ src/pageComponents/HomePage/defaultData/ourActivityData.ts
@@ -3,4 +3,9 @@ import image2 from '~assets/images/ourActivity/activity-image2.png';
 import image3 from '~assets/images/ourActivity/activity-image3.png';
 import image4 from '~assets/images/ourActivity/activity-image4.png';
 
-export const images = [image1, image2, image3, image4];
+export const images = [
+	{ id: 'image1', src: image1 },
+	{ id: 'image2', src: image2 },
+	{ id: 'image3', src: image3 },
+	{ id: 'image4', src: image4 },
+];
